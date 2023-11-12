f941c895d36f5fd792939823dff67b61509b1ea2
David <zurek.dav@gmail.com>
2023-11-12 22:49:03 +0100

    Install graphql packages & setup apollo graphql server

---
+++ src/resolvers.ts
@@ -0,0 +1,9 @@
+export const resolvers = {
+	Query: {
+		hello: () => {
+			return {
+				bla: "Hello"
+			}
+		}
+	}
+}
---
+++ src/typeDefs.ts
@@ -0,0 +1,8 @@
+export const typeDefs = `#graphql
+	type Query {
+		hello: Test
+	}
+	type Test {
+		bla: String
+	}
+`
